b8ff31d24f0afcd9b93a67904c0bb7d4a84f1a1d
jonblower <jonblower@f020452a-5e1f-0410-8fa4-ae2b6fb27d5e>
2009-02-04 08:49:09 +0000

    Creating branch for integration of Dave Crossman's transect code
    git-svn-id: https://ncwms.svn.sourceforge.net/svnroot/ncwms@498 f020452a-5e1f-0410-8fa4-ae2b6fb27d5e

---
+++ branches/transect-20090204/src/java/uk/ac/rdg/resc/ncwms/datareader/HorizontalGrid.java
@@ -0,0 +1,297 @@
+/*
+ * Copyright (c) 2007 The University of Reading
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University of Reading, nor the names of the
+ *    authors or contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package uk.ac.rdg.resc.ncwms.datareader;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.geotools.referencing.CRS;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.NoSuchAuthorityCodeException;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.cs.CoordinateSystemAxis;
+import org.opengis.referencing.operation.MathTransform;
+import org.opengis.referencing.operation.TransformException;
+import ucar.nc2.constants.AxisType;
+import ucar.unidata.geoloc.LatLonPoint;
+import ucar.unidata.geoloc.LatLonPointImpl;
+import uk.ac.rdg.resc.ncwms.controller.GetMapDataRequest;
+import uk.ac.rdg.resc.ncwms.exceptions.InvalidCrsException;
+import uk.ac.rdg.resc.ncwms.metadata.Regular1DCoordAxis;
+
+/**
+ * A Grid of points onto which data is to be projected.  This is the grid that
+ * is defined by the request CRS, the width, height and bounding box.
+ *
+ * @author Jon Blower
+ * $Revision$
+ * $Date$
+ * $Log$
+ */
+public class HorizontalGrid
+{
+    private static final Logger logger = LoggerFactory.getLogger(HorizontalGrid.class);
+    public static final String PLATE_CARREE_CRS_CODE = "CRS:84";
+    public static final CoordinateReferenceSystem PLATE_CARREE_CRS;
+    public static final List<String> SUPPORTED_CRS_CODES = new ArrayList<String>();
+    
+    private int width;      // Width of the grid in pixels
+    private int height;     // Height of the grid in pixels
+    private double[] bbox;  // Array of four floats representing the bounding box
+    private String crsCode; // String representing the CRS
+    private CoordinateReferenceSystem crs;
+    private MathTransform transformToLatLon;
+    private MathTransform latLonToCrs;
+    
+    private double[] xAxisValues;
+    private double[] yAxisValues;
+    
+    private Regular1DCoordAxis gridXAxis;
+    private Regular1DCoordAxis gridYAxis;
+    
+    static
+    {
+        // Find the supported CRS codes
+        // I think this is the appropriate method to get all the CRS codes
+        // that we can support
+        for (Object codeObj : CRS.getSupportedCodes("urn:ogc:def"))
+        {
+            SUPPORTED_CRS_CODES.add((String)codeObj);
+        }
+        try
+        {
+            PLATE_CARREE_CRS = CRS.decode(PLATE_CARREE_CRS_CODE, true); // force longitude-first
+        }
+        catch (Exception ex)
+        {
+            throw new ExceptionInInitializerError("Can't find CRS:84");
+        }
+    }
+    
+    /**
+     * Creates a HorizontalGrid from the parameters in the given GetMapDataRequest
+     * 
+     * @throws InvalidCrsException if the given CRS code is not recognized
+     * @throws Exception if there was an internal error.
+     * @todo check validity of the bounding box?
+     */
+    public HorizontalGrid(GetMapDataRequest dr) throws InvalidCrsException, Exception
+    {
+        this(dr.getCrsCode(), dr.getWidth(), dr.getHeight(), dr.getBbox());
+    }
+    
+    /**
+     * Creates a HorizontalGrid.
+     * 
+     * @param crsCode Code for the CRS of the grid
+     * @param width Width of the grid in pixels
+     * @param height Height of the grid in pixels
+     * @param bbox Bounding box of the grid in the units of the given CRS
+     * @throws InvalidCrsException if the given CRS code is not recognized
+     * @throws Exception if there was an internal error.
+     * @todo check validity of the bounding box?
+     */
+    public HorizontalGrid(String crsCode, int width, int height, double[] bbox)
+        throws InvalidCrsException, Exception
+    {
+        try
+        {
+            logger.debug("Getting CRS object for code {}", crsCode);
+            // The "true" means "force longitude-first axis order"
+            this.crs = CRS.decode(crsCode, true);
+            logger.debug("Finding transform for CRS {} to Plate Carree", crsCode);
+            // Get a converter to convert the grid's CRS to lat-lon
+            // The "true" means "lenient", i.e. ignore datum shifts.  This 
+            // is necessary to prevent "Bursa wolf parameters required"
+            // errors (Some CRSs, including British National Grid, fail if
+            // we are not "lenient".)
+            this.transformToLatLon = CRS.findMathTransform(
+                this.crs, PLATE_CARREE_CRS, true);
+            logger.debug("Found transform to Plate Carree");
+            logger.debug("Finding transform from Plate Carree to CRS {}", crsCode);
+            // Get a converter to convert lat-lon to the grid's CRS
+            // The "true" means "lenient", i.e. ignore datum shifts.  This 
+            // is necessary to prevent "Bursa wolf parameters required"
+            // errors (Some CRSs, including British National Grid, fail if
+            // we are not "lenient".)
+            this.latLonToCrs = CRS.findMathTransform(
+                PLATE_CARREE_CRS, this.crs, true);
+            logger.debug("Found transform from Plate Carree");
+        }
+        catch (NoSuchAuthorityCodeException ex)
+        {
+            logger.error("No CRS with code {} can be found", crsCode);
+            throw new InvalidCrsException(crsCode);
+        }
+        catch (FactoryException ex)
+        {
+            logger.error("FactoryException when creating CRS {} or its " +
+                "transform to Plate Carree", crsCode);
+            throw ex; // This is an internal error 
+        }
+        this.crsCode = crsCode;
+        this.width = width;
+        this.height = height;
+        this.bbox = bbox;
+        
+        // Now calculate the values along the x and y axes of this grid
+        double dx = (this.bbox[2] - this.bbox[0]) / this.width;
+        this.xAxisValues = new double[this.width];
+        for (int i = 0; i < this.xAxisValues.length; i++)
+        {
+            this.xAxisValues[i] = this.bbox[0] + (i + 0.5) * dx;
+        }
+        this.gridXAxis = new Regular1DCoordAxis(this.bbox[0], dx, this.width,
+            this.isLatLon() ? AxisType.Lon : AxisType.GeoX);
+        
+        double dy = (this.bbox[3] - this.bbox[1]) / this.height;
+        this.yAxisValues = new double[this.height];
+        for (int i = 0; i < this.yAxisValues.length; i++)
+        {
+            // The y axis is flipped
+            this.yAxisValues[i] = this.bbox[1] + (this.height - i - 0.5) * dy;
+        }
+        this.gridYAxis = new Regular1DCoordAxis(this.bbox[1], dy, this.height,
+            this.isLatLon() ? AxisType.Lat : AxisType.GeoY);
+        logger.debug("Created HorizontalGrid object for CRS {}", crsCode);
+    }
+
+    public int getWidth()
+    {
+        return this.width;
+    }
+
+    public int getHeight()
+    {
+        return this.height;
+    }
+
+    public double[] getBbox()
+    {
+        return this.bbox;
+    }
+
+    /**
+     * @return array of points along the x axis in this coordinate
+     * reference system
+     */
+    public double[] getXAxisValues()
+    {
+        return this.xAxisValues;
+    }
+
+    /**
+     * @return array of points along the y axis in this coordinate
+     * reference system
+     */
+    public double[] getYAxisValues()
+    {
+        return this.yAxisValues;
+    }
+
+    public CoordinateReferenceSystem getCoordinateReferenceSystem()
+    {
+        return this.crs;
+    }
+    
+    /**
+     * @return true if the given coordinate pair is valid for this CRS
+     */
+    public boolean isPointValidForCrs(double x, double y)
+    {
+        CoordinateSystemAxis xAxis = this.crs.getCoordinateSystem().getAxis(0);
+        CoordinateSystemAxis yAxis = this.crs.getCoordinateSystem().getAxis(1);
+        if (x < xAxis.getMinimumValue() || x > xAxis.getMaximumValue() ||
+            y < yAxis.getMinimumValue() || y > yAxis.getMaximumValue())
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public int getSize()
+    {
+        return this.width * this.height;
+    }
+    
+    /**
+     * Transforms the given x-y point to a LatLonPoint.
+     * @throws TransformException if the required transformation could not be performed
+     */
+    public LatLonPoint transformToLatLon(double x, double y) throws TransformException
+    {
+        // We know x must go first in this array because we selected
+        // "force longitude-first" when creating the CRS for this grid
+        double[] point = new double[]{x, y};
+        // Transform to lat-lon in-place
+        this.transformToLatLon.transform(point, 0, point, 0, 1);
+        return new LatLonPointImpl(point[1], point[0]);
+    }
+    
+    /**
+     * Transforms the given lat-lon point to a coordinate pair {x,y} within
+     * this grid.
+     * @param latLonPoint
+     * @return the coordinate pair, or null if this point is outside the grid.
+     */
+    public int[] latLonToGridCoords(double longitude, double latitude) throws TransformException {
+        double x, y;
+        if (this.isLatLon()) {
+            x = longitude;
+            y = latitude;
+        } else {
+            // Transform the lat-lon point into the CRS of this grid
+            double[] point = new double[]{longitude, latitude};
+            this.latLonToCrs.transform(point, 0, point, 0, 1);
+            x = point[0];
+            y = point[1];
+        }
+        int i = this.gridXAxis.getIndex(x);
+        int j = this.gridYAxis.getIndex(y);
+        if (i < 0 || j < 0) return null;
+        else return new int[]{i,j};
+    }
+    
+    /**
+     * @return true if this grid is a lat-lon grid
+     */
+    public final boolean isLatLon()
+    {
+        return this.transformToLatLon.isIdentity();
+    }
+
+    public String getCrsCode()
+    {
+        return crsCode;
+    }
+}
---
+++ branches/transect-20090204/src/java/uk/ac/rdg/resc/ncwms/metadata/MetadataStore.java
@@ -0,0 +1,314 @@
+/*
+ * Copyright (c) 2007 The University of Reading
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University of Reading, nor the names of the
+ *    authors or contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package uk.ac.rdg.resc.ncwms.metadata;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import uk.ac.rdg.resc.ncwms.config.Config;
+import uk.ac.rdg.resc.ncwms.config.Dataset;
+import uk.ac.rdg.resc.ncwms.config.NcwmsContext;
+import uk.ac.rdg.resc.ncwms.exceptions.LayerNotDefinedException;
+import uk.ac.rdg.resc.ncwms.utils.WmsUtils;
+
+/**
+ * Interface describing a persistent store of metadata
+ *
+ * @author Jon Blower
+ * $Revision$
+ * $Date$
+ * $Log$
+ */
+public abstract class MetadataStore
+{
+    
+    // This is set by Spring and is needed so that subclasses know where to
+    // store metadata
+    protected NcwmsContext ncwmsContext;
+    
+    // This is set by Config.readConfig() once the config information has been
+    // read.  It is needed to allow the methods that return Layers to set 
+    // the Dataset objects to which Layers belong.
+    protected Config config;
+    
+    /**
+     * Subclasses can override this method to provide initialization code, which
+     * is called after injection of all fields by Spring (i.e. the context object
+     * will be set before calling this method).
+     * This default method does nothing.
+     */
+    public void init() throws Exception {}
+    
+    /**
+     * Gets a Layer object from the metadata store.  This is a convenience
+     * method that wraps this.getLayer(), checking for valid input and throwing a
+     * LayerNotDefinedException if the layer is not present in the store.
+     * @param uniqueLayerName The unique Id of the layer, as returned by
+     * WmsUtils.createUniqueLayerName().
+     */
+    public final Layer getLayerByUniqueName(String uniqueLayerName)
+        throws LayerNotDefinedException, Exception
+    {
+        try
+        {
+            String[] els = WmsUtils.parseUniqueLayerName(uniqueLayerName);
+            Layer layer = this.getLayer(els[0], els[1]);
+            if (layer == null)
+            {
+                throw new LayerNotDefinedException(uniqueLayerName);
+            }
+            return layer;
+        }
+        catch(ParseException pe)
+        {
+            throw new LayerNotDefinedException(uniqueLayerName);
+        }
+    }
+    
+    /**
+     * Gets a Layer object from a dataset
+     * @param datasetId The ID of the dataset to which the layer belongs
+     * @param layerId The unique ID of the layer within the dataset
+     * @return The corresponding Layer, or null if there is no corresponding
+     * layer in the store.
+     * @throws Exception if an error occurs reading from the persistent store
+     */
+    public abstract Layer getLayer(String datasetId, String layerId)
+        throws Exception;
+    
+    /**
+     * Gets all the Layers that belong to a dataset
+     * @param datasetId The unique ID of the dataset, as defined in the config
+     * file
+     * @return a Collection of Layer objects that belong to this dataset
+     * @throws Exception if an error occurs reading from the persistent store
+     */
+    public abstract Collection<? extends Layer> getLayersInDataset(String datasetId)
+        throws Exception;
+    
+    /**
+     * Sets the Layers that belong to the dataset with the given id, overwriting
+     * all previous layers in the dataset.  This method should also update
+     * the lastUpdateTime for the dataset (to harmonize with this.getLastUpdateTime()).
+     * @param datasetId The ID of the dataset.
+     * @param layers The Layers that belong to the dataset.  Maps layer IDs
+     * (unique within a dataset) to Layer objects.
+     * @throws Exception if an error occurs writing to the persistent store
+     */
+    public abstract void setLayersInDataset(String datasetId, Map<String, ? extends Layer> layers)
+        throws Exception;
+    
+    /**
+     * Finds the unique X coordinate axes and maps each one to a list of Layers
+     * that use the axis
+     */
+    protected static Map<CoordAxis, List<Layer>> findUniqueXAxes(Collection<Layer> layers)
+    {
+        Map<CoordAxis, List<Layer>> xAxes = new HashMap<CoordAxis, List<Layer>>();
+        for (Layer layer : layers)
+        {
+            // See if we have an equivalent x axis already in the Map.  This will
+            // call the equals() methods on the CoordAxis objects.  Perhaps
+            // we can use the method xAxes.containsKey() here, but I'm not sure: I think
+            // this relies on the hashCode, which we have not implemented yet.
+            boolean found = false;
+            for (CoordAxis axis : xAxes.keySet())
+            {
+                if (axis.equals(layer.getXaxis()))
+                {
+                    found = true;
+                    // Now add this layer to the list of layers that use this axis
+                    xAxes.get(axis).add(layer);
+                }
+            }
+            if (!found)
+            {
+                // We must create a new entry in the hashtable
+                List<Layer> layerList = new ArrayList<Layer>();
+                layerList.add(layer);
+                xAxes.put(layer.getXaxis(), layerList);
+            }
+        }
+        return xAxes;
+    }
+    
+    /**
+     * Finds the unique Y coordinate axes and maps each one to a list of Layers
+     * that use the axis
+     */
+    protected static Map<CoordAxis, List<Layer>> findUniqueYAxes(Collection<Layer> layers)
+    {
+        Map<CoordAxis, List<Layer>> yAxes = new HashMap<CoordAxis, List<Layer>>();
+        for (Layer layer : layers)
+        {
+            // See if we have an equivalent y axis already in the Map.  This will
+            // call the equals() methods on the CoordAxis objects.  Perhaps
+            // we can use the method yAxes.containsKey() here, but I'm not sure: I think
+            // this relies on the hashCode, which we have not implemented yet.
+            boolean found = false;
+            for (CoordAxis axis : yAxes.keySet())
+            {
+                if (axis.equals(layer.getYaxis()))
+                {
+                    found = true;
+                    // Now add this layer to the list of layers that use this axis
+                    yAxes.get(axis).add(layer);
+                }
+            }
+            if (!found)
+            {
+                // We must create a new entry in the hashtable
+                List<Layer> layerList = new ArrayList<Layer>();
+                layerList.add(layer);
+                yAxes.put(layer.getYaxis(), layerList);
+            }
+        }
+        return yAxes;
+    }
+    
+    /**
+     * Finds unique arrays of z values and maps each one to a list of Layers
+     * that use the axis
+     */
+    protected static Map<double[], List<Layer>> findUniqueZAxes(Collection<Layer> layers)
+    {
+        Map<double[], List<Layer>> zAxes = new HashMap<double[], List<Layer>>();
+        for (Layer layer : layers)
+        {
+            // See if we have an equivalent z axis already in the Map.  This will
+            // call the equals() methods on the CoordAxis objects.  Perhaps
+            // we can use the method zAxes.containsKey() here, but I'm not sure: I think
+            // this relies on the hashCode, which we have not implemented yet.
+            boolean found = false;
+            for (double[] zValues : zAxes.keySet())
+            {
+                if (Arrays.equals(layer.getZvalues(), zValues))
+                {
+                    found = true;
+                    // Now add this layer to the list of layers that use this axis
+                    zAxes.get(zValues).add(layer);
+                }
+            }
+            if (!found)
+            {
+                // We must create a new entry in the hashtable
+                List<Layer> layerList = new ArrayList<Layer>();
+                layerList.add(layer);
+                zAxes.put(layer.getZvalues(), layerList);
+            }
+        }
+        return zAxes;
+    }
+    
+    /**
+     * Finds unique Lists of TimestepInfo objects and maps each one to a list of Layers
+     * that use the axis
+     */
+    protected static Map<List<TimestepInfo>, List<Layer>> findUniqueTAxes(Collection<Layer> layers)
+    {
+        Map<List<TimestepInfo>, List<Layer>> tAxes = new HashMap<List<TimestepInfo>, List<Layer>>();
+        for (Layer layer : layers)
+        {
+            // See if we have an equivalent t axis already in the Map.  This will
+            // call the equals() methods on the CoordAxis objects.  Perhaps
+            // we can use the method tAxes.containsKey() here, but I'm not sure: I think
+            // this relies on the hashCode, which we have not implemented yet.
+            boolean found = false;
+            for (List<TimestepInfo> tAxis : tAxes.keySet())
+            {
+                if (tAxis.equals(layer.getTimesteps()))
+                {
+                    found = true;
+                    // Now add this layer to the list of layers that use this axis
+                    tAxes.get(tAxis).add(layer);
+                }
+            }
+            if (!found)
+            {
+                // We must create a new entry in the hashtable
+                List<Layer> layerList = new ArrayList<Layer>();
+                layerList.add(layer);
+                tAxes.put(layer.getTimesteps(), layerList);
+            }
+        }
+        return tAxes;
+    }
+    
+    /**
+     * @return the time of the last update of the dataset with the given id,
+     * or null if the dataset has not yet been loaded into this store.  If an
+     * error occurs loading the last update time (which should be unlikely)
+     * implementing classes should log the error and return null.
+     */
+    public abstract Date getLastUpdateTime(String datasetId);
+    
+    /**
+     * Sets the Dataset property on the given layer.  Checks for Vector
+     * layers, setting the dataset property on the component layers too.
+     */
+    protected static void addDatasetProperty(Layer layer, Dataset ds)
+    {
+        ((LayerImpl)layer).setDataset(ds);
+        if (layer instanceof VectorLayer)
+        {
+            VectorLayer vecLayer = (VectorLayer)layer;
+            ((LayerImpl)vecLayer.getEastwardComponent()).setDataset(ds);
+            ((LayerImpl)vecLayer.getNorthwardComponent()).setDataset(ds);
+        }
+    }
+    
+    /**
+     * Called by Spring to clean up this store. Subclasses should override if
+     * necessary.
+     */
+    public void close() throws Exception {}
+
+    /**
+     * Called by Config.readConfig() to set the config object containing
+     * the configuration of this ncWMS server.
+     */
+    public void setConfig(Config config)
+    {
+        this.config = config;
+    }
+
+    /**
+     * Called by Spring to inject the context object
+     */
+    public void setNcwmsContext(NcwmsContext ncwmsContext)
+    {
+        this.ncwmsContext = ncwmsContext;
+    }
+    
+}
