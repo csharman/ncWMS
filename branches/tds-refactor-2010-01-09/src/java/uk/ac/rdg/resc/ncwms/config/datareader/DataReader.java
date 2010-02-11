90d252133e41797c63647bdbd2e839300df99477
jonblower <jonblower@f020452a-5e1f-0410-8fa4-ae2b6fb27d5e>
2010-02-11 20:27:01 +0000

    Moved expandGlobExpression to DataReader to avoid dependency of "core" ncWMS code on jakarta-oro
    git-svn-id: https://ncwms.svn.sourceforge.net/svnroot/ncwms@688 f020452a-5e1f-0410-8fa4-ae2b6fb27d5e

--- branches/tds-refactor-2010-01-09/src/java/uk/ac/rdg/resc/ncwms/config/datareader/DataReader.java
+++ branches/tds-refactor-2010-01-09/src/java/uk/ac/rdg/resc/ncwms/config/datareader/DataReader.java
@@ -30,12 +30,14 @@ package uk.ac.rdg.resc.ncwms.config.datareader;
 
 import uk.ac.rdg.resc.ncwms.coords.PointList;
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import org.apache.oro.io.GlobFilenameFilter;
 import uk.ac.rdg.resc.ncwms.config.Dataset;
 import uk.ac.rdg.resc.ncwms.config.LayerImpl;
 import uk.ac.rdg.resc.ncwms.coords.HorizontalPosition;
@@ -168,7 +170,7 @@ public abstract class DataReader
         {
             // The dataset's location represents locally-held data so we do
             // a glob expansion
-            List<File> files = WmsUtils.expandGlobExpression(dataset.getLocation());
+            List<File> files = expandGlobExpression(dataset.getLocation());
             if (files.size() == 0)
             {
                 throw new Exception(dataset.getLocation() + " does not match any files");
@@ -195,6 +197,87 @@ public abstract class DataReader
      */
     protected abstract void findAndUpdateLayers(String location,
         Map<String, LayerImpl> layers) throws Exception;
+
+        /**
+     * Expands a glob expression to give a List of absolute paths to files.  This
+     * method recursively searches directories, allowing for glob expressions like
+     * {@code "c:\\data\\200[6-7]\\*\\1*\\A*.nc"}.
+     * @return a a List of absolute paths to files matching the given glob
+     * expression
+     * @throws IllegalArgumentException if the glob expression does not represent
+     * an absolute path (according to {@code new File(globExpression).isAbsolute()}).
+     * @author Mike Grant, Plymouth Marine Labs; Jon Blower
+     */
+    public static List<File> expandGlobExpression(String globExpression)
+    {
+        // Check that the glob expression represents an absolute path.  Relative
+        // paths would cause unpredictable and platform-dependent behaviour so
+        // we disallow them.
+        File globFile = new File(globExpression);
+        if (!globFile.isAbsolute())
+        {
+            throw new IllegalArgumentException("Dataset location must be an absolute path");
+        }
+
+        // Break glob pattern into path components.  To do this in a reliable
+        // and platform-independent way we use methods of the File class, rather
+        // than String.split().
+        List<String> pathComponents = new ArrayList<String>();
+        while (globFile != null)
+        {
+            // We "pop off" the last component of the glob pattern and place
+            // it in the first component of the pathComponents List.  We therefore
+            // ensure that the pathComponents end up in the right order.
+            File parent = globFile.getParentFile();
+            // For a top-level directory, getName() returns an empty string,
+            // hence we use getPath() in this case
+            String pathComponent = parent == null ? globFile.getPath() : globFile.getName();
+            pathComponents.add(0, pathComponent);
+            globFile = parent;
+        }
+
+        // We must have at least two path components: one directory and one
+        // filename or glob expression
+        List<File> searchPaths = new ArrayList<File>();
+        searchPaths.add(new File(pathComponents.get(0)));
+        int i = 1; // Index of the glob path component
+
+        while(i < pathComponents.size())
+        {
+            FilenameFilter globFilter = new GlobFilenameFilter(pathComponents.get(i));
+            List<File> newSearchPaths = new ArrayList<File>();
+            // Look for matches in all the current search paths
+            for (File dir : searchPaths)
+            {
+                if (dir.isDirectory())
+                {
+                    // Workaround for automounters that don't make filesystems
+                    // appear unless they're poked
+                    // do a listing on searchpath/pathcomponent whether or not
+                    // it exists, then discard the results
+                    new File(dir, pathComponents.get(i)).list();
+
+                    for (File match : dir.listFiles(globFilter))
+                    {
+                        newSearchPaths.add(match);
+                    }
+                }
+            }
+            // Next time we'll search based on these new matches and will use
+            // the next globComponent
+            searchPaths = newSearchPaths;
+            i++;
+        }
+
+        // Now we've done all our searching, we'll only retain the files from
+        // the list of search paths
+        List<File> files = new ArrayList<File>();
+        for (File path : searchPaths)
+        {
+            if (path.isFile()) files.add(path);
+        }
+        return files;
+    }
 
     /**
      * Returns an ArrayList of null values of the given length
